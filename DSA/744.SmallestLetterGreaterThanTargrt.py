cea37930e2ed89e78c5d421aec44e9e909698968
Noor Kallur <noor.klur@gmail.com>
2024-04-06 16:58:58 +0530

    added another solution for 744

--- DSA/744.SmallestLetterGreaterThanTargrt.py
+++ DSA/744.SmallestLetterGreaterThanTargrt.py
@@ -16,3 +16,20 @@ def smallestLetterGreaterThanTarget(letters, target):
     return letters[s]
 
 print(smallestLetterGreaterThanTarget(["a", "c", "c", "c", "d"], "c"))
+
+# better code from dev
+def smallestLetterGreaterThanTarget(letters, target):
+    length = len(letters)
+    s = 0
+    e = length-1
+    if letters[e] < target:
+        return letters[s]
+    while s<=e:
+        mid = int((s+e)/2)
+        if letters[mid] > target:
+            e = mid - 1
+        else:
+            s = mid + 1
+    return letters[s % length]
+
+print(smallestLetterGreaterThanTarget(["a", "c", "c", "c", "d"], "c"))
