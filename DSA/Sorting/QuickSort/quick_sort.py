53d9dc05ff578f256ad0d9790defc39b78f3598f
Noor Kallur <noor.klur@gmail.com>
2024-07-20 16:42:13 +0530

    quick Sort algortithm

---
+++ DSA/Sorting/QuickSort/quick_sort.py
@@ -0,0 +1,31 @@
+def swap(i , j, nums):
+    temp = nums[i]
+    nums[i] = nums[j]
+    nums[j] = temp
+    
+    
+def quick_sort(nums, low, high):
+    if low>= high:
+        return
+    s = low
+    e = high
+    m = (s+e)//2
+    piv = nums[m]
+    
+    while s <=e:
+        while nums[s] < piv: # finding the left large element
+            s +=1
+        while nums[e] > piv: # finding the right small element
+            e -=1
+        
+        if s<=e: # after every parse for finding elements make sure s and e does not cross each other
+            swap(s, e, nums)
+            s +=1
+            e -=1
+    quick_sort(nums, low, e)
+    quick_sort(nums, s, high)
+    
+
+arr = [5, 4, 3, 2, 1]
+quick_sort(arr, 0, len(arr)-1)
+print(arr)
