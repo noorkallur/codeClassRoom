22d49640c7f3b43c559aeb2814cbbd3d1da026d3
Noor Kallur <noor.klur@gmail.com>
2024-10-09 00:07:56 +0530

    155.Minstack

---
+++ DSA/Stack/155.Minstack.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/min-stack/
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.min_stack:
+            val = min(self.min_stack[-1], val)
+        self.min_stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+    
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+    
+    
+obj = MinStack()
+obj.push(-2)
+obj.push(0)
+obj.push(-3)
+print(obj.getMin())
+obj.pop()
+print(obj.getMin())
