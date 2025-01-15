2af106dcf40e76dd75342bd577c88f8f5a63c334
Noor Kallur <noor.klur@gmail.com>
2025-01-16 00:38:16 +0530

    adding one more logic to search in duplicate rotated array

--- DSA/BinarySearch/SearchInDuplicateRotatedArray.py
+++ DSA/BinarySearch/SearchInDuplicateRotatedArray.py
@@ -24,6 +24,31 @@ def pivotIndexDuplicateRotatedArray(arr):
             e = mid -1
     return -1
 
+def pivotIndexDuplicateRotatedArray(nums):
+    s = 0
+    e = len(nums) -1
+    
+    while s <= e:
+        mid = (s+e)//2   
+        if nums[mid] > nums[mid + 1]:
+            return mid + 1
+        if mid != 0 and nums[mid] < nums[mid - 1]:
+            return mid
+        
+        if nums[mid] == nums[s] == nums[len(nums) -1]: # trimming if s == last == mid
+            if nums[mid] != nums[e]:
+                s = mid + 1
+            else:
+                s  += 1
+                e  -= 1
+            continue
+        
+        if nums[mid] > nums[len(nums) -1]:
+            s = mid + 1
+        else:
+            e = mid - 1
+    return - 1
+
 print(pivotIndexDuplicateRotatedArray([9, 9, 9, 1, 2, 9, 9, 9, 9, 9]))#2
 print(pivotIndexDuplicateRotatedArray([10, 11, 12, 13, 1, 2, 3, 4, 5, 6, 7, 8])) #3
 print(pivotIndexDuplicateRotatedArray([1, 2, 3, 4, 5, 6, 7, 8])) #-1
