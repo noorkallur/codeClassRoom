68e77ef6ce67790556b4b14eb021798aa7071bd6
Noor Kallur <noor.klur@gmail.com>
2025-01-20 22:14:20 +0530

    17.LetterCobinationsPhoneNumber

---
+++ DSA/Recursion/17.LetterCobinationsPhoneNumber.py
@@ -0,0 +1,37 @@
+# https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+def letterCombinations(digits: str):
+    if len(digits) == 0:
+        return [] 
+    hash_map = {
+        "2":"abc",
+        "3":"def",
+        "4":"ghi",
+        "5":"jkl",
+        "6":"mno",
+        "7":"pqrs",
+        "8":"tuv",
+        "9":"wxyz"}
+    
+    def permuations(digits, ps, res):
+        if len(digits) == 0:
+            print(f"finalnode:{ps}")
+            res.append(ps)
+            return 
+        us = hash_map[digits[0]]
+        # add one char to ps 
+        print(f"entering the iteration for us:{us}")
+        for j in range(0, len(us)):
+            print(f"entering j iteration {us[j]}")
+            print(f"ps: {ps+us[j]}")
+            permuations(digits[1:], ps+us[j], res)
+    
+    ps = ""
+    res = []
+    permuations(digits, ps, res) 
+    return res  
+
+lc = "23"
+print(letterCombinations(lc))
+                
+
+       
