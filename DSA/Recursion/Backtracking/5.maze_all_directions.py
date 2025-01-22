4de56c4f6a4b5d74d3cdced81d006cf8eeda4262
Noor Kallur <noor.klur@gmail.com>
2025-01-23 00:24:17 +0530

    backtracking definiton added

--- DSA/Recursion/Backtracking/5.maze_all_directions.py
+++ DSA/Recursion/Backtracking/5.maze_all_directions.py
@@ -1,10 +1,19 @@
-# use recursion to find the paths for a obstacle maze
+# use recursion to find all the paths for a maze
 # Rules: you can move any direction
 #  thinking processs:
 # why cant i use the maze_right_down_diag_ret_paths.py with left and up direction calls?
 # because if you add left and up, its going to be stuck in a loop like DUDUDUDUD.... so on
 # to prevent this we must not travel the path already traversed. how to make it... make the cell false(obstacle creation) as soon as we pass it
 # there is a problem.. how to revert this false back to true. because other recursion call may also land on the same cell.. after return call make the cell back to TRUE
+# now this is called Backtracking
+
+# Backtracking is a problem-solving technique that involves trying out different solutions and undoing (or "backtracking") steps when you hit a dead end. Think of it like exploring a maze:
+# 1.Try a path: You start at the entrance and choose a direction to go.
+# 2.Check progress: If you reach a dead end, you go back to the last decision point.
+# 3.Try another path: You then try a different direction from there.
+# 4.Repeat: You keep doing this until you find the exit or determine there's no solution.
+# It's like trial and error, but with a systematic way of undoing steps to explore all possible options. This method is often used in puzzles and games, like solving Sudoku or the N-Queens problem.
+
 
 def mazePaths(boolMaze):
     def pathFinder(boolMaze, row, col, pathCoord, res):
@@ -54,10 +63,10 @@ bool_maze = [
     [1, 1, 1],
     [1, 1, 1]
 ]
-bool_maze = [ 
-    [1, 1, 1],
-    [1, 1, 1]
-]
+# bool_maze = [ 
+#     [1, 1, 1],
+#     [1, 1, 1]
+# ]
 
 ans = mazePaths(bool_maze)
 
