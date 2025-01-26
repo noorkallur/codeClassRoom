6cbbe9e6350e99b2e31a6f18c8f2b7a363cf5bdf
Noor Kallur <noor.klur@gmail.com>
2025-01-27 00:06:37 +0530

    finally found the solution for 92 on my own in one pass

--- DSA/LinkedList/problems/4.92.ReverseLinkedList2.py
+++ DSA/LinkedList/problems/4.92.ReverseLinkedList2.py
@@ -60,6 +60,150 @@ class Solution:
         #reverse the whole linked list
 
 
+    def reverseBetween(self, left, right):
+        """
+            Reverses a sublist of a linked list from position `left` to `right`.
 
+            Parameters:
+            left (int): The starting position of the sublist to be reversed (1-indexed).
+            right (int): The ending position of the sublist to be reversed (1-indexed).
+
+            Returns:
+            None: The linked list is modified in place.
+
+            Description:
+            This function reverses the nodes of a linked list from position `left` to `right`. 
+            It uses a dummy node to handle edge cases where the reversal starts at the head of the list. 
+            The function iterates through the list, reversing the pointers of the nodes between `left` and `right`. 
+            After the reversal, it reconnects the reversed sublist with the rest of the list.
+
+            Edge Cases:
+            - If `left` is equal to `right`, the list remains unchanged.
+            - If `left` or `right` are out of bounds (i.e., less than 1 or greater than the length of the list), the function should handle these cases appropriately:
+                - If `left` < 1, it should be set to 1.
+                - If `right` > length of the list, it should be set to the length of the list.
+            - If the list is empty, the function should return immediately without making any changes.
+            - If `left` is greater than `right`, the function should return immediately as it is an invalid range.
+            
+            Example:
+            Given the linked list 1 -> 2 -> 3 -> 4 -> 5 and left = 2, right = 4,
+            the function will modify the list to 1 -> 4 -> 3 -> 2 -> 5.
+        """
+        
+        if left == right:
+            return
+        
+        dummy_head = Node(None)  # Dummy node to handle edge cases
+        dummy_head.next = self.head
+        
+        curr_node = dummy_head
+        position = 0
+        prev_node = None
+        left_side = None
+        reverse_end = None
+        
+        while curr_node:
+            if position == left:  # Mark the start of the reversal
+                left_side = prev_node
+                reverse_end = curr_node
+            
+            if position == right:  # End of the reversal
+                rend = curr_node.next
+                curr_node.next = prev_node  # Reverse the current node, this is the final reversal
+                
+                # Connect the reversed sublist with the rest of the list
+                if rend and left != 1:
+                    left_side.next = curr_node
+                    reverse_end.next = rend
+                elif rend and left == 1:
+                    reverse_end.next = rend
+                    self.head = curr_node
+                elif not rend and left != 1:
+                    left_side.next = curr_node
+                    reverse_end.next = None
+                else:
+                    self.head = curr_node
+                    reverse_end.next = None
+                
+                return  # Exit as we have completed the reversal
+            
+            next_node = curr_node.next
+            if position >= left:  # Reverse the nodes between left and right
+                curr_node.next = prev_node
+            prev_node = curr_node    
+            position += 1
+            curr_node = next_node
+ 
+ 
+# ll1 = LinkedList()
+# ll1.add_node_at_end(1)
+# ll1.add_node_at_end(2)
+# ll1.add_node_at_end(3)
+# ll1.add_node_at_end(4) 
+# ll1.add_node_at_end(5)
+
+# ll1.print_linked_list()
+# ll1.reverseBetween(3, 4) 
+# ll1.print_linked_list()
+
+# ll1 = LinkedList()
+# ll1.add_node_at_end(1)
+# ll1.add_node_at_end(2)
+# ll1.add_node_at_end(3)
+# ll1.add_node_at_end(4) 
+# ll1.add_node_at_end(5)
+
+# ll1.print_linked_list()
+# ll1.reverseBetween(2, 4) 
+# ll1.print_linked_list()   
+
+# ll1 = LinkedList()
+# ll1.add_node_at_end(5)
+
+# ll1.print_linked_list()
+# ll1.reverseBetween(1, 1) 
+# ll1.print_linked_list()        
+        
+
+# ll1 = LinkedList()
+# ll1.add_node_at_end(1)
+# ll1.add_node_at_end(2)
+# ll1.add_node_at_end(3)
+
+# ll1.print_linked_list()
+# ll1.reverseBetween(1, 2) 
+# ll1.print_linked_list()   
+
+
+# ll1 = LinkedList()
+# ll1.add_node_at_end(1)
+# ll1.add_node_at_end(2)
+# ll1.add_node_at_end(3)
+
+# ll1.print_linked_list()
+# ll1.reverseBetween(2, 3) 
+# ll1.print_linked_list()   
+
+
+# ll1 = LinkedList()
+# ll1.add_node_at_end(1)
+# ll1.add_node_at_end(2)
+# ll1.add_node_at_end(3)
+
+# ll1.print_linked_list()
+# ll1.reverseBetween(3, 3) 
+# ll1.print_linked_list()   
+
+
+# ll1 = LinkedList()
+# ll1.add_node_at_end(1)
+# ll1.add_node_at_end(2)
+# ll1.add_node_at_end(3)
+# ll1.add_node_at_end(4) 
+# ll1.add_node_at_end(5)
+
+# ll1.print_linked_list()
+# ll1.reverseBetween(4, 5) 
+# ll1.print_linked_list() 
         
         
