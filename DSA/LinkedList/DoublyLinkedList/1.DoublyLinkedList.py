13d3ff32900d47fb22cca4ed6d9ff78a7839e847
Noor Kallur <noor.klur@gmail.com>
2025-01-28 00:51:11 +0530

    rewrote dll from memory

--- DSA/LinkedList/DoublyLinkedList/1.DoublyLinkedList.py
+++ DSA/LinkedList/DoublyLinkedList/1.DoublyLinkedList.py
@@ -73,6 +73,74 @@ dll.insert_at_end(4)
 dll.print_dll()
 dll.insert_at_index(8, 2)
 dll.print_dll()
-dll.delete_at_index(2)
+dll.delete_at_index(0)
 print(f"result")
 dll.print_dll()
+
+
+# rewrote again from memory
+
+class DoublyLinkedList2():
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def print_linked_list(self):
+        itr =self.head
+        ll =[]
+        while itr:
+            ll.append(itr.data)
+            itr =itr.next
+        print(ll)
+        
+        itr = self.tail
+        ll =[]
+        while itr:
+            ll.append(itr.data)
+            itr =itr.prev
+        print(ll)
+     
+    def insert_at_end(self, val):
+        if self.head == None:
+            self.head = Node(val)
+            return
+        
+        curr_node = self.head
+        while curr_node.next:
+            curr_node = curr_node.next
+        new_node =  Node(val)
+        curr_node.next = new_node
+        new_node.prev = curr_node 
+        self.tail = new_node
+    
+    
+    def remove_at_end(self):
+        self.tail = self.tail.prev
+        self.tail.next = None
+    
+    def remove_begining(self):
+        if self.head:
+            next_node = self.head.next
+            self.head = next_node
+            if self.head == None:
+                self.tail = None
+                return
+            self.head.prev = None
+                
+    def delete_at_index(self, index):
+        if index == 0:
+            self.remove_begining()
+            return
+        curr_node = self.head
+        curr_idx = 0
+        while curr_node:
+            if curr_idx == index:
+                if curr_node == self.tail:
+                    self.remove_at_end()
+                    return
+                curr_node.prev.next = curr_node.next
+                curr_node.next.prev = curr_node.prev
+            curr_node = curr_node.next
+            curr_idx +=1
+            
+        print(f"index not found")    
