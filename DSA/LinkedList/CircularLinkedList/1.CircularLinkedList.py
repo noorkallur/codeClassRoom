4a90da097e84c732fd37bd64a6910470cc0201de
Noor Kallur <noor.klur@gmail.com>
2025-01-28 22:58:42 +0530

    added circular linked list from memory

--- DSA/LinkedList/CircularLinkedList/1.CircularLinkedList.py
+++ DSA/LinkedList/CircularLinkedList/1.CircularLinkedList.py
@@ -121,3 +121,96 @@ cll.delete_value(1)
 cll.printcll()
 cll.delete_value(2)
 cll.printcll()
+
+
+# rewrote from memory
+class CircularLinkedList2():
+    def __init__(self):
+        self.head = None
+
+    def print_linked_list(self):
+        itr =self.head
+        ll =[]
+        while itr.next != self.head:
+            ll.append(itr.data)
+            itr =itr.next
+        ll.append(itr.data)
+        print(ll)
+        
+        
+    def insert_at_end(self, val):
+        if self.head == None:
+            self.head = Node(val)
+            self.head.next = self.head
+            return
+        
+        curr_node = self.head
+        while curr_node.next != self.head:
+            curr_node = curr_node.next
+            
+        #now its pointing to the tail
+        new_end = Node(val)
+        new_end.next = self.head
+        curr_node.next = new_end
+
+    def insert_at_begining(self, val):
+        if self.head == None:
+            self.head = Node(val)
+            self.head.next =self.head
+            return
+        curr_node = self.head
+        while curr_node.next != self.head:
+            curr_node = curr_node.next
+        
+        #pointing to tail    
+        new_head = Node(val)
+        new_head.next = self.head
+        curr_node.next = new_head
+        self.head = new_head
+        
+    def insert_at_index(self, index, val):
+        if index == 0:
+            self.insert_at_begining(val)
+            return
+            
+        curr_node = self.head
+        curr_idx = 0
+        while curr_node.next != self.head:
+            curr_idx +=1
+            curr_node = curr_node.next
+            if curr_idx+1 == index:
+                new_node = Node(val)
+                temp_next = curr_node.next
+                curr_node.next = new_node
+                new_node.next = temp_next
+                return
+        
+        print(f"Index not found")
+
+    def delete_head(self):
+        if self.head == None:
+            return
+        
+        curr_node =self.head
+        while curr_node.next != self.head:
+            curr_node = curr_node.next
+            
+        # now at tail
+        curr_node.next = curr_node.next.next
+        self.head = curr_node.next
+
+    def delete_at_index(self, index):
+        if index == 0:
+            self.delete_head()
+            return
+            
+        curr_idx = 0
+        curr_node = self.head
+        while curr_node.next != self.head:
+            curr_idx +=1
+            curr_node = curr_node.next
+            if curr_idx+1 == index:
+                curr_node.next = curr_node.next.next
+                return
+        
+        print(f"Index not found")
