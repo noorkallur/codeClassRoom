3613786bbcfe28d74b42e7b5b45330024fe7fa24
Noor Kallur <noor.klur@gmail.com>
2025-02-07 00:34:11 +0530

    re-did nQueens

--- DSA/Recursion/Backtracking/Nqueen/1.Nqueens.py
+++ DSA/Recursion/Backtracking/Nqueen/1.Nqueens.py
@@ -55,3 +55,60 @@ def Nqueen(nrows, ncols):
 print(Nqueen(8, 8)) # 92 ways
 print(Nqueen(4, 4)) # 2  ways
 # Time complexity is pretty interesting here please take a look at the image first nqueen_q_arrangements.png first 
+
+
+###
+# revisisted the question
+def isQsafe(board, row, col):
+    # verical 
+    r= row
+    while r>=0:
+        if board[r][col]:
+            return False
+        r -=1
+    
+    # left diag
+    c= col
+    r= row
+    while c >= 0 and r >= 0:
+        if board[r][c]:
+            return False
+        c-=1
+        r-=1
+    # right diag
+    c= col
+    r= row
+    while c <= len(board)-1 and r >=0:
+        if board[r][c]:
+            return False
+        c+=1
+        r-=1
+        
+    return True
+
+def printBoard(board):
+    for ls in board:
+        tmp = [l for l in ls]
+        print(tmp)
+    print("")
+       
+def nQueens(board):
+    
+    def qPlacer(board, row, col):
+        if col > len(board)-1:
+            return 0
+
+        if row == len(board[0]):
+            printBoard(board)
+            return 1
+        count = 0
+        if isQsafe(board, row, col):
+            board[row][col] = 1
+            count +=qPlacer(board, row+1, 0)
+            board[row][col] = 0
+
+        count+=qPlacer(board, row, col+1)
+        
+        return count
+            
+    return qPlacer(board, 0, 0)
