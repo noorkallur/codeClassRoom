dd5409026ae2abd37b0dc55a3558bcb58c147df7
Noor Kallur <noor.klur@gmail.com>
2025-02-15 15:50:56 +0530

    3.LevelOrderSuccessor

---
+++ DSA/Trees/problems/3.LevelOrderSuccessor.py
@@ -0,0 +1,76 @@
+# google question
+# https://youtu.be/9D-vP-jcc-Y?t=2048&si=X1WzdnHPOEzaQTIB
+# Given a tree find the next node of the given target node
+# look at the 3.LevelOrderSuccessor.png to understand better
+from collections import deque 
+
+class Node:
+    def __init__(self, value: int):
+        self.val = value
+        self.left = None
+        self.right = None
+        
+class BinaryTree:
+    def __init__(self):
+        root_node = int(input("Insert ROOT node: "))
+        self.root = Node(root_node)
+        self.nodePlacer(self.root)
+        
+    def nodePlacer(self, parentNode):
+        left = input(f"Do you want to place left of {parentNode.val} (y/n): ")
+        if left == "y":
+            left_value = int(input("Enter left value: "))
+            parentNode.left = Node(left_value)
+            self.nodePlacer(parentNode.left)
+            
+        right = input(f"Do you want to place right of {parentNode.val} (y/n): ")
+        if right == "y":
+            right_value = int(input("Enter right value: "))
+            parentNode.right = Node(right_value)
+            self.nodePlacer(parentNode.right)
+            
+    def prettyDisplay(self, parentNode, level):
+        if parentNode == None:
+            return # as soon as we dont have anything print that many tabs and pring number at the end
+        
+        # find the last right node
+        self.prettyDisplay(parentNode.right, level+1)
+        
+        if level != 0:
+            for i in range(level-1):
+                print("|\t", end="")
+            print(f"|------>{parentNode.val}")
+        else:
+            print(parentNode.val)
+         
+        # left in that level 
+        self.prettyDisplay(parentNode.left, level+1)
+
+    
+    def level_order_successor(self, root, target):
+        q = deque()
+        q.append(root)
+        target_found = False
+        
+        while len(q):
+            node = q.popleft()
+            if target_found == True:
+                return node.val
+            
+            if target == node.val:
+                target_found = True
+                
+            if node.left != None:
+                q.append(node.left)
+            if node.right != None:
+                q.append(node.right)
+    
+    
+bt = BinaryTree()
+bt.prettyDisplay(bt.root, 0)
+print(bt.level_order_successor(bt.root, 3))
+print(bt.level_order_successor(bt.root, 9))
+print(bt.level_order_successor(bt.root, 20))
+print(bt.level_order_successor(bt.root, 15))
+print(bt.level_order_successor(bt.root, 7))
+        
