f7ecca453de7c0eef62ea83497091133299d918a
Noor Kallur <noor.klur@gmail.com>
2025-02-25 23:18:59 +0530

    added a dev solution

--- DSA/Trees/problems/17.236.LowestCommonAncestorofaBinaryTree.py
+++ DSA/Trees/problems/17.236.LowestCommonAncestorofaBinaryTree.py
@@ -37,3 +37,23 @@ class Solution:
         for p in p_list:
             if p in q_list:
                 return p
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if root == None:
+            return None
+        
+        if root == p or root == q:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        
+        if left and not right:
+            return left
+        else:
+            return root
+        
+        
+               
