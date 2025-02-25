4491bec4e700d1aade03bb933b12e3a4ff628c8a
Noor Kallur <noor.klur@gmail.com>
2025-02-26 00:08:44 +0530

    230. Kth Smallest Element in a BST

--- DSA/Trees/problems/18.230.KthSmallestElementinaBST.py
+++ DSA/Trees/problems/18.230.KthSmallestElementinaBST.py
@@ -4,6 +4,10 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
+# Intuition is that we keep appending to the list, as soon the list is equals to k then return from recursion
+# how to exit recursion:
+# return true only when list has the k elements otherwise false
 class Solution:
     def kthSmallest(self, root:TreeNode, k: int) -> int:
         def kthSmallest_helper(root, k):
@@ -11,12 +15,41 @@ class Solution:
                 return False
             
             if kthSmallest_helper(root.left, k):
-                num_list.append(root.val)
-                if len(num_list) == k:
-                    return True
+                return True
+            # check if we are found the kth element    
+            num_list.append(root.val)
+            if len(num_list) == k:
+                return True
                 
-            return kthSmallest_helper(root.left, k)
+            return kthSmallest_helper(root.right, k)
         
         num_list = []
         kthSmallest_helper(root, k)
         return num_list[k-1]
+    
+
+# Different return mechanism
+class Solution:
+    def kthSmallest(self, root:TreeNode, k: int) -> int:
+        def kthSmallest_helper(root, k):
+            if root == None:
+                return None
+            
+            left = kthSmallest_helper(root.left, k)
+            if left!= None:
+                return left
+
+            # check if we are found the kth element    
+            num_list.append(root.val)
+            if len(num_list) == k:
+                return num_list[k-1]
+                
+            return kthSmallest_helper(root.right, k)
+
+        num_list = []
+        return kthSmallest_helper(root, k)
+    
+
+
+         
+        
