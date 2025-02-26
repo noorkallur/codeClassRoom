e3ae22862cf249d626a04a6276b6088e15d76328
Noor Kallur <noor.klur@gmail.com>
2025-02-26 18:16:42 +0530

    updated

--- DSA/Trees/problems/19.105.ConstructBinaryTreefromPreorderandInorderTraversal.py
+++ DSA/Trees/problems/19.105.ConstructBinaryTreefromPreorderandInorderTraversal.py
@@ -4,35 +4,26 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-
 class Solution:
     def buildTree(self, preorder, inorder) -> TreeNode:
-        def buildTree_helper(preorder, inorder):
-            if len(preorder) == 0 and len(inorder) == 0:
-                return
+        if len(preorder) == 0 and len(inorder) == 0:
+            return
+    
+        root = TreeNode(preorder[0])
         
-            root = TreeNode(preorder[0])
-            
-            index = 0
-            for i in range(len(inorder)):
-                if preorder[0] == inorder[i]:
-                    index = i
-                    break
-            
-            root.left = self.buildTree(preorder[1:i+1], inorder[:i])
-            root.right= self.buildTree(preorder[i+1:], inorder[i+1:])
-
-            return root
-
-        inorder_index_map = {}
-        for i in range(len(inorder)+1):
-            inorder_index_map[inorder[i]] = i
+        for i in range(len(inorder)):
+            if preorder[0] == inorder[i]:
+                break
         
-        return buildTree_helper(preorder, inorder)
+        root.left = self.buildTree(preorder[1:i+1], inorder[:i])
+        root.right= self.buildTree(preorder[i+1:], inorder[i+1:])
+
+        return root
+        
     
 # some other solution
 class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+    def buildTree(self, preorder, inorder) -> TreeNode:
         def buildTree_helper(preorder, inorder):
             print(f"pre: {preorder}")
             print(f" imor: {inorder}")
