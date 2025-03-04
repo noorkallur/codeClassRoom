f29d335e115360cd25e53be997cded440644c428
Noor Kallur <noor.klur@gmail.com>
2025-03-04 16:32:19 +0530

    not using a string and list

--- DSA/Trees/problems/22.129. Sum Root to Leaf Numbers.py
+++ DSA/Trees/problems/22.129. Sum Root to Leaf Numbers.py
@@ -28,4 +28,33 @@ class Solution:
 
         return sum
         
-        
+
+# another solution
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.total_sum = 0
+
+    def sumNumbers(self, root) -> int:
+        def sumNumbers_helper(root, num_rl):
+            if root == None:
+                return
+
+            if root.left == None and root.right == None:
+                num_rl = (10*num_rl)+root.val
+                print(num_rl)
+                self.total_sum +=num_rl
+
+            num_rl = (10*num_rl)+root.val
+            print(num_rl)
+            sumNumbers_helper(root.left, num_rl)
+            sumNumbers_helper(root.right, num_rl)
+
+        sumNumbers_helper(root, 0)
+        return self.total_sum
+
