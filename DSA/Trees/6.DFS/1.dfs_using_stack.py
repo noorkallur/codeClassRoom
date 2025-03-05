1ebe3b9691d541bb919989ffdfe156c24b215e11
Noor Kallur <noor.klur@gmail.com>
2025-03-05 17:06:29 +0530

    DFS using stack

---
+++ DSA/Trees/6.DFS/1.dfs_using_stack.py
@@ -0,0 +1,69 @@
+class Node:
+    def __init__(self, value: int):
+        self.val = value
+        self.left = None
+        self.right = None
+        self.next = None
+        
+class BinaryTree:
+    def __init__(self):
+        root_node = int(input("Insert ROOT node: "))
+        self.root = Node(root_node)
+        self.nodePlacer(self.root)
+        
+    def nodePlacer(self, parentNode):
+        left = input(f"Do you want to place left of {parentNode.val} (y/n): ")
+        if left == "y":
+            left_value = int(input("Enter left value: "))
+            parentNode.left = Node(left_value)
+            self.nodePlacer(parentNode.left)
+            
+        right = input(f"Do you want to place right of {parentNode.val} (y/n): ")
+        if right == "y":
+            right_value = int(input("Enter right value: "))
+            parentNode.right = Node(right_value)
+            self.nodePlacer(parentNode.right)
+            
+    def prettyDisplay(self, parentNode, level):
+        if parentNode == None:
+            return # as soon as we dont have anything print that many tabs and pring number at the end
+        
+        # find the last right node
+        self.prettyDisplay(parentNode.right, level+1)
+        
+        if level != 0:
+            for i in range(level-1):
+                print("|\t", end="")
+            next_node = parentNode.next
+            if next_node!=None:
+                next_node = next_node.val
+            print(f"|------>{parentNode.val}({next_node})")
+        else:
+            print(f"{parentNode.val}({parentNode.next})")
+        # left in that level 
+        self.prettyDisplay(parentNode.left, level+1)
+
+
+class Solution:
+    def dfs_using_stack(self, root):
+        stk = []
+        stk.append(root)
+        tree_nums = []
+        while len(stk):
+            node = stk.pop()
+            tree_nums.append(node.val)
+
+            if node.right:
+               stk.append(node.right) 
+
+            if node.left:
+               stk.append(node.left) 
+
+        print(tree_nums)
+            
+
+bt = BinaryTree()
+bt.prettyDisplay(bt.root, 0)
+sln = Solution()
+sln.dfs_using_stack(bt.root)
+
