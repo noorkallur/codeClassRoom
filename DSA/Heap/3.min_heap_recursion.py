ba3b52fa2ae430be7365c2843cb4ec4fe0adb65f
Noor Kallur <noor.klur@gmail.com>
2025-03-27 23:17:48 +0530

    min heap using recursion

---
+++ DSA/Heap/3.min_heap_recursion.py
@@ -0,0 +1,62 @@
+class Min_Heap:
+    def __init__(self):
+        self.data = []
+
+    def parent(self, index):
+        return (index + 1) //2 -1
+    
+    def left(self, index):
+        return (index + 1) *2 -1
+    
+    def right(self, index):
+        return (index +1)*2 
+    
+
+    def insert(self, val):
+        self.data.append(val)
+        self.up_heap(len(self.data)-1)
+    
+    def up_heap(self, index):
+        if index == 0:
+            return
+        
+        p_index = self.parent(index)
+        if self.data[p_index] > self.data[index]:
+            self.data[p_index], self.data[index] = self.data[index], self.data[p_index]
+            self.up_heap(p_index)
+
+    def remove_at_index(self, index):
+        if index == len(self.data) -1:
+            self.data.pop()
+            return
+        
+        self.data[index] = self.data.pop()
+
+        self.down_heap(index)
+
+    def down_heap(self, index):
+        left_idx = self.left(index)
+        right_idx = self.left(index)
+        min_index = index
+        if left_idx < len(self.data) and self.data[left_idx] < self.data[min_index]:
+            min_index = left_idx
+
+        if right_idx < len(self.data) and self.data[right_idx] < self.data[min_index]:
+            min_index = right_idx
+
+        if min_index != index:
+            #swap
+            self.data[index], self.data[min_index] = self.data[min_index], self.data[index]
+            self.down_heap(min_index)
+
+
+min_heap = Min_Heap()
+min_heap.insert(10)
+min_heap.insert(20)
+min_heap.insert(30)
+min_heap.insert(1)
+print(min_heap.data)
+min_heap.remove_at_index(0)
+print(min_heap.data)
+min_heap.remove_at_index(1)
+print(min_heap.data)
