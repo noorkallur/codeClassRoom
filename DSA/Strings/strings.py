0e7a3b604d528dfc0b658533473653a5c44d9a77
Noor Kallur <noor.klur@gmail.com>
2025-04-24 22:21:11 +0530

    added strings

---
+++ DSA/Strings/strings.py
@@ -0,0 +1,14 @@
+# len(): Returns the length of the string.
+# lower(): Converts all characters in the string to lowercase.
+# upper(): Converts all characters in the string to uppercase.
+# strip(): Removes any leading and trailing whitespace.
+# split(): Splits the string into a list of substrings based on a delimiter.
+# join(): Joins elements of a list into a single string, with a specified separator.
+# replace(): Replaces occurrences of a substring with another substring.
+# find(): Returns the index of the first occurrence of a substring, or -1 if not found.
+# startswith(): Checks if the string starts with a specified substring.
+# endswith(): Checks if the string ends with a specified substring.
+# isalpha(): Checks if the string contains only alphabetic characters.
+# isdigit(): Checks if the string contains only digits.
+# isalnum(): Checks if the string contains only alphanumeric characters.
+# format(): Formats the string using placeholders.
