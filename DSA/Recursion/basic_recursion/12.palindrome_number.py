311bfa4a53e7ac8b2a456d43acdfea6390e43e77
Noor Kallur <noor.klur@gmail.com>
2025-05-03 13:29:18 +0530

    revisit

--- DSA/Recursion/basic_recursion/12.palindrome_number.py
+++ DSA/Recursion/basic_recursion/12.palindrome_number.py
@@ -13,3 +13,29 @@ def IsPalindrome(num):
         return True
     else:
         False
+
+# rewrote the function
+#is number a palindrome
+# reverse a number and check if its the same
+def reverseNumber(num, units):
+    if num <= 0:
+        return 0
+    
+    return (num%10 * units) + reverseNumber(num//10, units//10)
+    
+def isPalindrome(num):
+    # count the units
+    units = 1
+    c_num = num
+    while c_num>9:
+        c_num = c_num//10
+        units *=10
+
+    if num == reverseNumber(num, units):
+        return True
+    else:
+        return False
+
+
+print(isPalindrome(123))
+print(isPalindrome(1221))
