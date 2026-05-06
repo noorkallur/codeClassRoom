3745f479f936cf4a958be8008150615ef2962ef3
Noor Kallur <noor.klur@gmail.com>
2026-05-06 22:33:45 +0530

    updated

--- DSA/Recursion/basic_recursion/5.dice_combinations.py
+++ DSA/Recursion/basic_recursion/5.dice_combinations.py
@@ -1,25 +1,35 @@
 # how many combinations can num can be done throwing a dice
+
 # for eg , 2 can have [1, 1], [2]
 
+# for eg ,7
+# 1, 1, 1, 1, 1, 1, 1
+# 1, 1, 1, 1, 1, 2
+# 1, 1, 1, 1, 3
+# ...
+
 def diceCombinations(target):
-    def combinations(p, up, res):
-        if up == 0:
-            res.append(p)
+    result = []
+    currSol = []
+    def combinations(target):
+        if target == 0:
+            result.append(currSol[:])
+            currSol.clear()
             return
-        
-        for num in range(1, up+1):
-            # p.append(num) 
-            # combinations(p, up - num, res) # here p is modified, so it will mess up recursive calls
-            combinations(p + [num], up - num, res) # tip: inorder to preserve the p value at recursive calls dont modify in between directly send it in args
-            
-    res = []
-    p = []
-    combinations(p, target, res)
-    return res
+
+        for i in range(1, target+1):
+            subTarget = target - i
+            if subTarget < 0:
+                continue
+            currSol.append(i)
+            combinations(subTarget)
+    
+    combinations(target)
+    return result
     
 ans = diceCombinations(3)
 ans = diceCombinations(4)
-ans = diceCombinations(2)
+ans = diceCombinations(7)
 print(ans)
 # ans = diceCombinations(4)
 # print(ans)
