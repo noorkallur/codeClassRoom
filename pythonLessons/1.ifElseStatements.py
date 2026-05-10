fb27189caaa91c0c9904bba7d279c40cd764dab4
Noor Kallur <noor.klur@gmail.com>
2026-05-10 11:31:28 +0530

    updated

--- pythonLessons/1.ifElseStatements.py
+++ pythonLessons/1.ifElseStatements.py
@@ -1,28 +1,12 @@
-# weight = input("Weight: ")
-# lweight= weight.lower()
-# if lweight.find('l') != -1 :
-#     print(f'weight is {float(weight[:-1]) *0.45} kg')
-# elif lweight.find('k') != -1:
-#     print(f'weight is {float(weight[:-1])*(1/0.45)} lbs')
-# else:
-#     while(1):
-#         unit = input("Is it (L)bs or (K)g? ")
-#         if(unit.lower() == "l"):
-#             print(f'weight  is {float(weight)*0.45} kg')
-#             break
-#         elif(unit.lower() == "k"):
-#             print(f'weight  is {float(weight)*(1/0.45)} lbs')
-#             break
-#         else:
-#             print("enter the right unit, either l or k")
+Weight = "ali"
+weight = input("Weight:-") #90 KG, 90 lb
+lweight= weight.lower()
+unitIndex = lweight.find("k")
+print(unitIndex)
+if unitIndex == -1:
+    unitIndex = lweight.find("l")
+    print(f'weight is {float(weight[:unitIndex]) *0.45} kg')
+else:
+    print(f'weight is {float(weight[:unitIndex])*(1/0.45)} lbs')
 
-
-# short hand if else statements
-
-a = 10
-b = 2
-print("A") if a > b else ""
-
-c = 1 if a > b else 0
-print(c)
 
